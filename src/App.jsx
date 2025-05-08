628e663b6e7d5b9e6969786f19802b6e2e67db0e
Jaafar Ali <jaafar.ali@infostrategic.com>
2025-05-08 21:16:07 +0400

    set basename to browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import { BrowserRouter } from 'react-router';
-import AppRoutes from './routes/AppRoutes';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter } from "react-router";
+import AppRoutes from "./routes/AppRoutes";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="down-center">
       <Header />
-      <main style={{ minHeight: '80vh' }}>
+      <main style={{ minHeight: "80vh" }}>
         <AppRoutes />
       </main>
       <Footer />
